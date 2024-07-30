b694f10a3a71ce12098f5735bfe64c6b4988069e
DileeshaNUWANSARA <snuwansara123@gmail.com>
2024-07-30 15:18:09 +0530

    Initial commit to frontend-react

---
+++ internBridgeFrontEnd/internbridge-frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is frontend main Homepage InternBridge.
+      </header>
+    </div>
+  );
+}
+
+export default App;

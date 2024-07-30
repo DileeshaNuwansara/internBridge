2b9deba395356f13b324a2640775c974c81f2586
DileeshaNUWANSARA <snuwansara123@gmail.com>
2024-07-30 15:20:49 +0530

    Initial commit to backend SpringBoot

---
+++ internBridgeBackEnd/internbridge-backend/src/main/java/com/internbridge/internbridge_backend/InternbridgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.internbridge.internbridge_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternbridgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternbridgeBackendApplication.class, args);
+	}
+
+}
---
+++ internBridgeBackEnd/internbridge-backend/src/test/java/com/internbridge/internbridge_backend/InternbridgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internbridge.internbridge_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternbridgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

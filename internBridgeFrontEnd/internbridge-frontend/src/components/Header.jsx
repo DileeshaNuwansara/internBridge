d0d176eb2042a356db30cb7ac08aa0f2b471c7f9
DileeshaNUWANSARA <snuwansara123@gmail.com>
2024-08-07 01:09:24 +0530

    folder update

---
+++ internBridgeFrontEnd/internbridge-frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+//import {Navbar, Nav, Container} from '';
+const Header =() => {
+  return (
+    <header>
+        <Navbar>
+
+        </Navbar>
+    </header>
+      
+  )
+}
+
+export default Header

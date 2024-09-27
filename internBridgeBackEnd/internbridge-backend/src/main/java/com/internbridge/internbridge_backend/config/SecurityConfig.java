a2ef673fbc24ce62d7232effcdef893c3bfa489e
DileeshaNUWANSARA <snuwansara123@gmail.com>
2024-09-27 09:03:24 +0530

    added security some codes

--- internBridgeBackEnd/internbridge-backend/src/main/java/com/internbridge/internbridge_backend/config/SecurityConfig.java
+++ internBridgeBackEnd/internbridge-backend/src/main/java/com/internbridge/internbridge_backend/config/SecurityConfig.java
@@ -1,2 +1,36 @@
-package com.internbridge.internbridge_backend.config;public class SecurityConfig {
+package com.internbridge.internbridge_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/oauth2/**", "/login/**", "/landing").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/oauth2/success", true)
+                )
+                .formLogin(withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/landing")
+                        .permitAll()
+                )
+                .csrf(csrf -> csrf.disable())// Disables CSRF protection
+                .build();
+
+        return http.build();
+    }
 }
